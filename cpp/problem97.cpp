691f73440be1bfbdbefedf0ca67455a15c19ba13
nurashid <rashid.nafi.ur@gmail.com>
2017-05-26 19:09:46 -0700

    Update problem97.cpp

--- cpp/problem97.cpp
+++ cpp/problem97.cpp
@@ -4,39 +4,30 @@ class Solution {
 #define vvvi vector<vector<vector<int>>>
 #define vvi vector<vector<int>>
 #define vi vector<int>
-    int isInterleaveUtil(string s1, string s2, string s3,
-            int i, int j, int k, vvvi& dp) {
+    
+        int isInterleaveUtil(string s1, string s2, string s3,
+                            int i, int j, int k, vvvi& dp) {
         if (i > s1.length() || j > s2.length() || k > s3.length())
             return 0;
         if (dp[i][j][k] == -1) {
+            dp[i][j][k] = 0;
             if (i == s1.length() and j == s2.length() and
-                    k == s3.length()) {
+                k == s3.length())
                 dp[i][j][k] = 1;
-                return 1;
-            }
             if (k != s3.length()) {
                 if (i != s1.length() and s1[i] == s3[k]
-                        and isInterleaveUtil(s1, s2, s3, i+1, j, k+1, dp)) {
+                    and isInterleaveUtil(s1, s2, s3, i+1, j, k+1, dp))
                     dp[i][j][k] = 1;
-                    return 1;
-                }
                 if (j != s2.length() and s2[j] == s3[k]
-                        and isInterleaveUtil(s1, s2, s3, i, j+1, k+1, dp)) {
+                    and isInterleaveUtil(s1, s2, s3, i, j+1, k+1, dp))
                     dp[i][j][k] = 1;
-                    return 1;
-                }
-            }
-
-        } else {
-            return dp[i][j][k];
+            }     
         }
-        dp[i][j][k] = 0;
-        return 0;
+        return dp[i][j][k];
     }
     public:
     int isInterleave(string s1, string s2, string s3) {
         vvvi dp(s1.length() + 1, vvi(s2.length() + 1, vi(s3.length() + 1, -1)));
-
         isInterleaveUtil(s1, s2, s3, 0, 0, 0, dp);
         return dp[s1.length()][s2.length()][s3.length()];
     }
